2117d25a9843029d89948286dda6c64e9a9a4af6
inoculisvestris <69584343+inoculisvestris@users.noreply.github.com>
2024-11-24 20:31:27 +0300

    Js moments fix

--- lesson-2/greeting.rb
+++ lesson-2/greeting.rb
@@ -8,11 +8,11 @@ def greeting
     puts "Введи свой возраст:"
     age = gets.chomp.to_i
 
-    if (age < 18) {
+    if (age < 18)
         puts "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
-    } else {
+    else
         puts "Привет, #{name} #{surname}. Самое время заняться делом!"
-    } end
+    end
 end 
 
 greeting
