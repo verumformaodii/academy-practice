faeb0d156bf79d5a87919a37cb8ad7dbfaeeae4e
inoculisvestris <69584343+inoculisvestris@users.noreply.github.com>
2024-11-24 20:31:04 +0300

    Create greeting.rb

---
+++ lesson-2/greeting.rb
@@ -0,0 +1,18 @@
+def greeting
+    puts "Введи своё имя:"
+    name = gets.chomp
+    
+    puts "Введи свою фамилию:"
+    surname = gets.chomp
+    
+    puts "Введи свой возраст:"
+    age = gets.chomp.to_i
+
+    if (age < 18) {
+        puts "Привет, #{name} #{surname}. Тебе меньше 18 лет, но начать учиться программировать никогда не рано"
+    } else {
+        puts "Привет, #{name} #{surname}. Самое время заняться делом!"
+    } end
+end 
+
+greeting
